9f94b9a823f175f2b79263f406a953dbe40b1fec
Akash Bhagat <bhagatakash0123@gmail.com>
2021-08-17 15:14:14 +0530

    initial commit

---
+++ store/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+class Store(models.Model):
+    user_id = models.PositiveIntegerField(unique=True)
+    banner = models.ImageField(upload_to='banners/')
+    profile_pic = models.ImageField(upload_to='profiles/')
+    store_name = models.CharField(max_length=250)
+    address1 = models.CharField(max_length=250)
+    address2 = models.CharField(max_length=250)
+    city = models.CharField(max_length=100)
+    zipcode = models.CharField(max_length=20)
+    country = models.CharField(max_length =100)
+    state = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=20)
+    email = models.EmailField()
